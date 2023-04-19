ce713479effef56ad3c917a8e20033c8f99e3229
hyeontae04 <127460027+hyeontae04@users.noreply.github.com>
2023-04-19 22:39:37 +0900

    Colaboratory를 통해 생성됨

---
+++ hw6.py
@@ -0,0 +1,85 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyMUATJ24Hm2BH2iL/rkvYC8",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/hyeontae04/202310968/blob/main/hw6.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 5,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/",
+          "height": 136
+        },
+        "id": "VPWGqtSqnavA",
+        "outputId": "124f0201-065d-4e70-fbe1-97cc023e83ec"
+      },
+      "outputs": [
+        {
+          "output_type": "error",
+          "ename": "SyntaxError",
+          "evalue": "ignored",
+          "traceback": [
+            "\u001b[0;36m  File \u001b[0;32m\"<ipython-input-5-b514ea7279e7>\"\u001b[0;36m, line \u001b[0;32m7\u001b[0m\n\u001b[0;31m    def display_multiplication_table2(n) :\u001b[0m\n\u001b[0m    ^\u001b[0m\n\u001b[0;31mSyntaxError\u001b[0m\u001b[0;31m:\u001b[0m invalid syntax\n"
+          ]
+        }
+      ],
+      "source": [
+        "def display_multiplication_table(n) :\n",
+        "  for i in range(1, 10):\n",
+        "   for n in range(2, 6) :\n",
+        "    print(f'fn x [i = fn * i:2d',\n",
+        "   print()\n",
+        "\n",
+        "def display_multiplication_table2(n) :\n",
+        " print('In')\n",
+        " for i in range(1, 10):\n",
+        "  for n in range(6, 10) :\n",
+        "     print(f'/n) x fil = (n * i:2d>',\n",
+        "  print()\n",
+        "\n",
+        "display_multiplication_table(2)\n",
+        "\n",
+        "display multiplication_table2(6)\n",
+        "\n",
+        "ultiplication_table(n) :\n",
+        "nge(1, 10):\n",
+        "range(2, 6) :\n",
+        "'fn> X fi) E in * i:2d)' end='It')\n",
+        "     \n",
+        "ultiplication_table2(n) :\n",
+        "\n",
+        "nge(1, 10):\n",
+        "range(6, 10) :\n",
+        "f'fn) x fi) = fn * i:2d)', end='It')\n",
+        "\n",
+        "display_multiplication_table(2)\n",
+        "\n",
+        "display_multiplication_table2(6)"
+      ]
+    }
+  ]
+}
